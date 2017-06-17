c8e8007b0252d3383b540cd98723da9f8d0db947
Son DANG <son@tinypulse.com>
2017-06-17 15:07:43 +0700

    Filter Food Item by Section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,10 +7,14 @@ class HomeController < ApplicationController
 
   def menu
     @sections = Section.order(:id)
-    if params[:section]
-      @food_items = FoodItem.where(section: params[:section])
-    else
+    if params[:section].blank?
       @food_items = FoodItem.all
+    else
+      @food_items = FoodItem.where(section: params[:section])
+    end
+
+    if params[:sort_column]
+      @food_items = @food_items.order("#{params[:sort_column]} #{params[:sort_direction]}")
     end
   end
 end

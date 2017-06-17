bfa2c4b0b15b21c5687cc120a5528302f3f14dbe
Son DANG <son@tinypulse.com>
2017-06-17 11:34:52 +0700

    Fiter items by section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,10 @@ class HomeController < ApplicationController
 
   def menu
     @sections = Section.order(:id)
-    @food_items = FoodItem.where(section: params[:section])
-    @top_food_items = FoodItem.last(5)
+    if params[:section]
+      @food_items = FoodItem.where(section: params[:section])
+    else
+      @food_items = FoodItem.all
+    end
   end
 end
